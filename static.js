80797afbcc530954ef147adfec8631696683f496
chrisw <chrisw@soe.ucsc.edu>
2014-08-25 17:15:40 -0700

    organize

--- static.js
+++ static.js
@@ -5,6 +5,93 @@
  */
 
 /**
+ * Get an object's attribute keys.
+ * @param {Object} obj
+ */
+function getKeys(obj) {
+    var keys = [];
+    for (var key in obj) {
+        keys.push(key);
+    }
+    return keys;
+}
+
+/**
+ * From https://dreaminginjavascript.wordpress.com/2008/08/22/eliminating-duplicates/
+ * @param {Object} arr
+ */
+function eliminateDuplicates(arr) {
+    var i, len = arr.length, out = [], obj = {};
+
+    for ( i = 0; i < len; i++) {
+        obj[arr[i]] = 0;
+    }
+    for (i in obj) {
+        out.push(i);
+    }
+    return out;
+}
+
+/**
+ * Keep items that appear multiple times.  Original order of items is lost.
+ */
+function keepReplicates(arr, threshold) {
+    var counts = {};
+    // tally counts
+    for (var i = 0; i < arr.length; i++) {
+        var value = arr[i];
+        if ( value in counts) {
+        } else {
+            counts[value] = 0;
+        }
+        counts[value]++;
+    }
+    // apply threshold
+    threshold = (threshold == null) ? 2 : threshold;
+    var outList = [];
+    for (var value in counts) {
+        if (counts[value] >= threshold) {
+            outList.push(value);
+        }
+    }
+    return outList;
+}
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+function isNumerical(val) {
+    var result = true;
+    if (val == null || val === "") {
+        return false;
+    }
+
+    // As per IEEE-754 spec, a nan checked for equality against itself will be unequal (in other words, nan != nan)
+    // ref: http://kineme.net/Discussion/DevelopingCompositions/CheckifnumberNaNjavascriptpatch
+    if (isNaN(val)) {
+        return false;
+    }
+    return result;
+}
+
+/**
+ * get the selected values of a list box control.
+ */
+function getListBoxSelectedValues(listboxElement) {
+    var selectedValues = new Array();
+    for (var i = 0; i < listboxElement.length; i++) {
+        var option = listboxElement[i];
+        if (option.selected) {
+            selectedValues.push(option.value);
+        }
+    }
+    return selectedValues;
+}
+
+// TODO date & time
+
+/**
  * MySQL style date
  */
 function getDateTime() {
@@ -62,97 +149,7 @@ function todaysDate() {
     return (months[month] + " " + date + ", " + year);
 }
 
-/**
- * Turn serializedJson string into a JSON object.
- */
-function parseJson(serializedJson) {
-    var deserializedJson = JSON && JSON.parse(serializedJson) || $.parseJSON(serializedJson);
-    return deserializedJson;
-}
-
-/**
- * Get a pretty JSON.
- */
-function prettyJson(object) {
-    return JSON.stringify(object, null, '\t');
-}
-
-/**
- * From https://dreaminginjavascript.wordpress.com/2008/08/22/eliminating-duplicates/
- * @param {Object} arr
- */
-function eliminateDuplicates(arr) {
-    var i, len = arr.length, out = [], obj = {};
-
-    for ( i = 0; i < len; i++) {
-        obj[arr[i]] = 0;
-    }
-    for (i in obj) {
-        out.push(i);
-    }
-    return out;
-}
-
-/**
- * Keep items that appear multiple times.  Original order of items is lost.
- */
-function keepReplicates(arr, threshold) {
-    var counts = {};
-    // tally counts
-    for (var i = 0; i < arr.length; i++) {
-        var value = arr[i];
-        if ( value in counts) {
-        } else {
-            counts[value] = 0;
-        }
-        counts[value]++;
-    }
-    // apply threshold
-    threshold = (threshold == null) ? 2 : threshold;
-    var outList = [];
-    for (var value in counts) {
-        if (counts[value] >= threshold) {
-            outList.push(value);
-        }
-    }
-    return outList;
-}
-
-/**
- * Get an object with UrlQueryString data.
- */
-function getQueryObj() {
-    var result = {};
-    var keyValuePairs = location.search.slice(1).split('&');
-
-    keyValuePairs.forEach(function(keyValuePair) {
-        keyValuePair = keyValuePair.split('=');
-        result[keyValuePair[0]] = decodeURIComponent(keyValuePair[1]) || '';
-    });
-
-    return result;
-}
-
-/**
- * Get the value of a parameter from the query string.  If parameter has not value or does not exist, return <code>null</code>.
- * From <a href='http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values'>here</a>.
- * @param {Object} name
- */
-function getQueryStringParameterByName(name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
-    var results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-/**
- * querySettings is an object to be stringified into the query string.
- * @param {Object} querySettings
- */
-function loadNewSettings(querySettings) {
-    var url = window.location.pathname + "?query=" + JSON.stringify(querySettings);
-    window.open(url, "_self");
-}
+// TODO DOM
 
 /**
  * Create an unattached div element
@@ -187,37 +184,84 @@ function lengthOfLongestString(arrayOfStrings) {
     return maxLength;
 }
 
-function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+// TODO URL and query strings
+
+/*
+ * Synchronous GET
+ */
+function getResponse(url) {
+    var status = null;
+    var xhr = null;
+    xhr = new XMLHttpRequest();
+    xhr.open("GET", url, false);
+    xhr.onload = function() {
+        status = xhr.status;
+        if (status != 200) {
+            console.log("xhr status: " + status + " for " + url);
+        }
+    };
+    xhr.send(null);
+    var response = null;
+    if (status == 200) {
+        response = xhr.responseText;
+    }
+    return response;
 }
 
-function isNumerical(val) {
-    var result = true;
-    if (val == null || val === "") {
-        return false;
-    }
+/**
+ * querySettings is an object to be stringified into the query string.
+ * @param {Object} querySettings
+ */
+function loadNewSettings(querySettings) {
+    var url = window.location.pathname + "?query=" + JSON.stringify(querySettings);
+    window.open(url, "_self");
+}
 
-    // As per IEEE-754 spec, a nan checked for equality against itself will be unequal (in other words, nan != nan)
-    // ref: http://kineme.net/Discussion/DevelopingCompositions/CheckifnumberNaNjavascriptpatch
-    if (isNaN(val)) {
-        return false;
-    }
+/**
+ * Get an object with UrlQueryString data.
+ */
+function getQueryObj() {
+    var result = {};
+    var keyValuePairs = location.search.slice(1).split('&');
+
+    keyValuePairs.forEach(function(keyValuePair) {
+        keyValuePair = keyValuePair.split('=');
+        result[keyValuePair[0]] = decodeURIComponent(keyValuePair[1]) || '';
+    });
+
     return result;
 }
 
 /**
- * get the selected values of a list box control.
+ * Get the value of a parameter from the query string.  If parameter has not value or does not exist, return <code>null</code>.
+ * From <a href='http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values'>here</a>.
+ * @param {Object} name
  */
-function getListBoxSelectedValues(listboxElement) {
-    var selectedValues = new Array();
-    for (var i = 0; i < listboxElement.length; i++) {
-        var option = listboxElement[i];
-        if (option.selected) {
-            selectedValues.push(option.value);
-        }
-    }
-    return selectedValues;
+function getQueryStringParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)");
+    var results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
+
+// TODO JSON
+
+/**
+ * Turn serializedJson string into a JSON object.
+ */
+function parseJson(serializedJson) {
+    var deserializedJson = JSON && JSON.parse(serializedJson) || $.parseJSON(serializedJson);
+    return deserializedJson;
+}
+
+/**
+ * Get a pretty JSON.
+ */
+function prettyJson(object) {
+    return JSON.stringify(object, null, '\t');
+}
+
+// TODO SVG paths
 
 /**
  * Returns SVG path data for a rectangle with rounded bottom corners.
@@ -289,6 +333,87 @@ function allAngledRectSvgPath(x, y, width, height) {
     return pathString;
 }
 
+// TODO SVG elements
+
+function createSvgRingElement(cx, cy, r, attributes) {
+    // https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
+    // (rx ry x-axis-rotation large-arc-flag sweep-flag x y)+
+
+    function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+        var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+        return {
+            x : centerX + (radius * Math.cos(angleInRadians)),
+            y : centerY + (radius * Math.sin(angleInRadians))
+        };
+    }
+
+    function describeArc(x, y, radius, startAngle, endAngle) {
+        var start = polarToCartesian(x, y, radius, endAngle);
+        var end = polarToCartesian(x, y, radius, startAngle);
+        var arcSweep = endAngle - startAngle <= 180 ? "0" : "1";
+        var d = ["M", start.x, start.y, "A", radius, radius, 0, arcSweep, 0, end.x, end.y].join(" ");
+        return d;
+    }
+
+    // TODO somehow the circle becomes invisible if using 0 to 360 degrees
+    var arcPath = describeArc(cx, cy, r, 0, 359.9);
+
+    var e = document.createElementNS(svgNamespaceUri, "path");
+    e.setAttributeNS(null, "d", arcPath);
+    if (attributes != null) {
+        for (var attribute in attributes) {
+            e.setAttributeNS(null, attribute, attributes[attribute]);
+        }
+    }
+    return e;
+}
+
+function createSvgCircleElement(cx, cy, r, attributes) {
+    var e = document.createElementNS(svgNamespaceUri, "circle");
+    e.setAttributeNS(null, "cx", cx);
+    e.setAttributeNS(null, "cy", cy);
+    e.setAttributeNS(null, 'r', r);
+    if (attributes != null) {
+        for (var attribute in attributes) {
+            e.setAttributeNS(null, attribute, attributes[attribute]);
+        }
+    }
+    return e;
+}
+
+function createSvgRectElement(x, y, rx, ry, width, height, attributes) {
+    var e = document.createElementNS(svgNamespaceUri, "rect");
+    e.setAttributeNS(null, "x", x);
+    e.setAttributeNS(null, "y", y);
+    e.setAttributeNS(null, "rx", rx);
+    e.setAttributeNS(null, "ry", ry);
+    e.setAttributeNS(null, "width", width);
+    e.setAttributeNS(null, "height", height);
+    if (attributes != null) {
+        for (var attribute in attributes) {
+            e.setAttributeNS(null, attribute, attributes[attribute]);
+        }
+    }
+    return e;
+}
+
+function createSvgImageElement(imageUrl, x, y, width, height, attributes) {
+    var e = document.createElementNS(svgNamespaceUri, "image");
+    e.setAttributeNS(xlinkUri, "href", imageUrl);
+    e.setAttributeNS(null, "x", x);
+    e.setAttributeNS(null, "y", y);
+    e.setAttributeNS(null, "width", width);
+    e.setAttributeNS(null, "height", height);
+    if (attributes != null) {
+        for (var attribute in attributes) {
+            e.setAttributeNS(null, attribute, attributes[attribute]);
+        }
+    }
+    return e;
+}
+
+// TODO cookies
+
 /**
  * Cookie methods taken from:
  * <ul>
@@ -326,16 +451,4 @@ function getCookie(name) {
 
 function deleteCookie(name) {
     setCookie(name, "", -1);
-}
-
-/**
- * Get an object's attribute keys.
- * @param {Object} obj
- */
-function getKeys(obj) {
-    var keys = [];
-    for (var key in obj) {
-        keys.push(key);
-    }
-    return keys;
 }
