20a6056ee4c98fc3d4713d5a166532318253906c
chrisw <chrisw@soe.ucsc.edu>
2016-05-03 13:51:07 -0700

    handle tab keydown events as text

--- utils.js
+++ utils.js
@@ -1334,6 +1334,28 @@ var utils = utils || {};
         return mi;
     };
 
+    // event handler for tab keydown events in textarea elements
+    // document.querySelector("textarea").addEventListener('keydown', u.handleTabsAsText, false);
+    u.handleTabsAsText = function(e) {
+        if (e.keyCode === 9) {// tab was pressed
+            // get caret position/selection
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+
+            var target = e.target;
+            var value = target.value;
+
+            // set textarea value to: text before caret + tab + text after caret
+            target.value = value.substring(0, start) + "\t" + value.substring(end);
+
+            // put caret at right position again (add one for the tab)
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            // prevent the focus lose
+            e.preventDefault();
+        }
+    };
+
 })(utils);
 
 // console.log('utils', utils);
